9357b56cc01fee011844b818988991220180ada6
fvn70 <fvn_reg@mail.ru>
2022-04-30 19:14:21 +0700

    Stage 2/5: Are you a designer

--- Minesweeper/task/src/App.js
+++ Minesweeper/task/src/App.js
@@ -4,15 +4,45 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Minesweeper is loading...
-        </p>
-      </header>
-    </div>
+      <div className="App">
+         <div className="Game">
+            <header className="App-header">
+                <p>Minesweeper</p>
+                <img src={logo} className="App-logo" alt="logo" />
+            </header>
+            <Controls />
+             <Field/>
+         </div>
+      </div>
   );
 }
 
 export default App;
+
+function Controls() {
+    return (
+        <div className="Panel">
+            <p className="FlagsCounter">10</p>
+            <button className="Reset">Reset</button>
+            <p className="Timer">0:00</p>
+        </div>
+    );
+}
+
+function Cell(prop) {
+    return (
+        <div className="cell">{prop.value}</div>
+    )
+}
+
+function Field() {
+    const arr = Array(72);
+    arr.fill('*');
+    const cells = arr.map((v, index) =>
+        <Cell key={index} value={v.value} />);
+    return (
+        <div className="Field">
+            {cells}
+        </div>
+    );
+}
