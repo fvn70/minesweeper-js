6b33a5ea300199e1f754efbf85a3b018f8ee9d67
fvn70 <fvn_reg@mail.ru>
2022-05-03 20:05:37 +0700

    Stage 3/5: The main characters cells

--- Minesweeper/task/src/App.js
+++ Minesweeper/task/src/App.js
@@ -29,20 +29,77 @@ function Controls() {
     );
 }
 
-function Cell(prop) {
-    return (
-        <div className="cell">{prop.value}</div>
-    )
-}
-
 function Field() {
-    const arr = Array(72);
-    arr.fill('*');
+    const arr = mines();
+    // arr.fill('*');
     const cells = arr.map((v, index) =>
-        <Cell key={index} value={v.value} />);
+        <Cell key={index} data={v} />);
     return (
         <div className="Field">
             {cells}
         </div>
     );
 }
+
+function mines() {
+    const arr = Array(72);
+    arr.fill('');
+    arr.fill('*', 0, 10);
+    for (let i = 0; i < 10; i++) {
+        let j = 10 + Math.floor(Math.random() * 62);
+        let tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+        console.log(i, j, arr[i], arr[j]);
+    }
+    return arr;
+}
+
+class Cell extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isClosed: true,
+            isMarked: false,
+            isMined: props.data === '*'
+        }
+        this.handleRClick = this.handleRClick.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.setState(prev => ({
+            isClosed: false
+        }));
+    }
+
+    handleRClick(e) {
+        e.preventDefault();
+        this.setState(prev => ({
+            isMarked: !prev.isMarked
+        }));
+    }
+
+    render() {
+        let cls = "cell";
+        if (!this.state.isClosed) {
+            cls += " cell-open"
+            if (this.state.isMined) {
+                cls += " cell-mine"
+            }
+        } else if (this.state.isMarked) {
+            cls += " cell-mark"
+        }
+        return (
+            <div className={cls}
+                 onClick={this.handleClick}
+                 onContextMenu={this.handleRClick}>
+                { this.state.isClosed
+                    ? this.state.isMarked ? '#' : ''
+                    : this.state.isMined ? '&' : ''
+                }
+            </div>
+        )
+    }
+}
+
